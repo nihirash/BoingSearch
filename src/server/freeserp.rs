881049cbe7de3a7717b5ed1ec38f0f16d74a3452
Aleksandr Sharikhin <anihirash@gmail.com>
2025-10-26 18:46:41 +0100

    random useragent for duckduckgo and main page requesting for more robust searches

--- src/server/freeserp.rs
+++ src/server/freeserp.rs
@@ -1,15 +1,26 @@
 use crate::server::search::Serp;
-use duckduckgo::browser::Browser;
+use duckduckgo::{browser::Browser, user_agents};
+use rand::prelude::IteratorRandom;
+use reqwest::redirect::Policy;
 
 pub async fn get_free_serp(query: String) -> anyhow::Result<Vec<Serp>> {
+    let user_agent = user_agents::USER_AGENTS
+        .values()
+        .choose(&mut rand::rng())
+        .cloned()
+        .unwrap_or(crate::USER_AGENT);
+
     let req = reqwest::Client::builder()
-        .user_agent(crate::USER_AGENT)
+        .cookie_store(true)
+        .user_agent(user_agent)
+        .redirect(Policy::limited(10))
         .build()?;
 
+    let _ = req.get("http://lite.duckduckgo.com/").send().await?;
     let browser = Browser::new(req);
 
     let r = browser
-        .lite_search(&query, "wt-WT", None, crate::USER_AGENT)
+        .lite_search(&query, "wt-WT", None, user_agent)
         .await?;
 
     let result: Vec<_> = r
@@ -27,7 +38,7 @@ pub async fn get_free_serp(query: String) -> anyhow::Result<Vec<Serp>> {
 
 #[tokio::test]
 async fn call_get_free_serp() -> anyhow::Result<()> {
-    let res = get_free_serp("Nihirash".to_string()).await?;
+    let res = get_free_serp("Amiga 40".to_string()).await?;
 
     println!("{res:#?}");
 
