2392c6e23fa2775028289e8a72663b24a9fec8da
Aleksandr Sharikhin <anihirash@gmail.com>
2025-11-02 21:27:41 +0100

    ua changed

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod server;
 use serde::{Deserialize, Serialize};
 use url::Url;
 
-pub const USER_AGENT: &str = "Mozilla/5.0 (compatible; IBrowse 3.0; AmigaOS4.0)";
+pub const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36";
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AppConfig {
